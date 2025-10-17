5917dcef9a1650cf78f4dbf31945ebbfb69a9ed0
alkingbibi <94806216+alkingbibi@users.noreply.github.com>
2025-10-17 22:19:50 +0300

    Update build.gradle.kts

--- yamani_app/android/app/build.gradle.kts
+++ yamani_app/android/app/build.gradle.kts
@@ -1,51 +1,46 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
     id("dev.flutter.flutter-gradle-plugin")
 }
-    
-val localProperties = java.util.Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localProperties.load(localPropertiesFile.reader())
-}
-    
-val flutterVersionCode = localProperties.getProperty("flutter.versionCode") ?: "1"
-val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
-    
+
 android {
     namespace = "com.example.yamani_app"
     compileSdk = 34
-    ndkVersion = "25.1.8937393"
-    
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    
+
     defaultConfig {
         applicationId = "com.example.yamani_app"
         minSdk = 21
         targetSdk = 34
-        versionCode = flutterVersionCode.toInt()
-        versionName = flutterVersionName
-        multiDexEnabled = true
+        versionCode = 1
+        versionName = "1.0"
     }
-    
+
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+        debug {
+            isMinifyEnabled = false
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    ndkVersion = "27.0.12077973"
+    buildFeatures {
+        viewBinding = true
+    }
 }
-    
+
 flutter {
     source = "../.."
 }
-    
-dependencies {}
